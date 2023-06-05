a7199dab9531f7abe70fdc08b0b8ddf481bb949d
Tomáš Bosák <tombos255@gmail.com>
2023-06-05 22:01:54 +0200

    Connected to mongoDB

---
+++ server.js
@@ -0,0 +1,35 @@
+// import dotenv
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// CORS
+app.use(cors());
+
+// Parse JSON
+app.use(bodyParser.json());
+
+// Routes
+app.get("/*", (req, res) => {
+	res.json({ message: "Invalid URL" });
+});
+
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
+    }
+);
+
+// Connect to MongoDB
+mongoose
+	.connect(process.env.DB_CONNECTION_DEV, { useNewUrlParser: true })
+	.then(() => console.log("Connected To MongoDB !"))
+	.catch((error) => console.error("Error Connecting To MongoDB", error));
+// Connect to MongoDB
+
