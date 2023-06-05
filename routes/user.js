59385217f92f32a2e888ecdb577b29c9a0a65664
Tomáš Bosák <tombos255@gmail.com>
2023-06-06 01:06:10 +0200

    SSL + Docker

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,21 @@ const User = require("../models/user_model");
 // Import .env variables
 require("dotenv/config");
 
-
 // Get all users
-router.get("/", async (req, res) => {
-    res.json({ message: "Users route" });
-});
+router.post("/", async (req, res) => {
+	try {
+		const userData = req.body;
 
+		const newUser = new User(userData);
+
+		const createdUser = await newUser.save();
+
+		res.json(createdUser);
+        res.redirect("/");
+	} catch (error) {
+		console.error("Error creating user:", error);
+		res.status(500).json({ error: "Failed to create user" });
+	}
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 // import dotenv
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const HOST = process.env.HOST || "0.0.0.0";
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+
+// SSL
+app.use(express.urlencoded({ extended: true, limit: "3mb" }));
+app.use(express.json({ limit: "3mb" }));
+
+// SSL options
+const options = {
+	key: fs.readFileSync("key.pem"),
+	cert: fs.readFileSync("cert.pem"),
+};
 
 // CORS
 app.use(cors());
@@ -15,27 +27,21 @@ app.use(cors());
 // Parse JSON
 app.use(bodyParser.json());
 
-
 // import routes
-const userRoute = require('./routes/user');
-
+const userRoute = require("./routes/user");
 
 // Use routes
-app.use('/users', userRoute);
-
-
-
+app.use("/users", userRoute);
 
 // Routes
 app.get("/*", (req, res) => {
 	res.json({ message: "Invalid URL" });
+	res.redirect("/");
 });
 
-
-app.listen(PORT, HOST, () => {
-    console.log(`Server running on http://${HOST}:${PORT}`);
-    }
-);
+https.createServer(options, app).listen(PORT, HOST, () => {
+	console.log(`Server running on http://${HOST}:${PORT}`);
+});
 
 // Connect to MongoDB
 mongoose
@@ -43,4 +49,3 @@ mongoose
 	.then(() => console.log("Connected To MongoDB !"))
 	.catch((error) => console.error("Error Connecting To MongoDB", error));
 // Connect to MongoDB
-
