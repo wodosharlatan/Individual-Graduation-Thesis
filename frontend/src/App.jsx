e29e69aa793758fb98fec8581280da5d59c40ab2
Tomáš Bosák <tombos255@gmail.com>
2023-12-04 23:02:37 +0100

    minor bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home/home';
 import Password_reset from './pages/password_reset/password_reset';
 import Password_reseted from "./pages/email_responses/password_reseted/password_reseted";
-import user_verified from "./pages/email_responses/user_verified/user_verified";
+import User_verified from "./pages/email_responses/user_verified/user_verified";
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/password-reset" element={<Password_reset/>}/>
         <Route path="/password-reset/:CODE" element={<Password_reseted/>}/>
-        <Route path="/verify/:CODE" element={<user_verified/>}/>
+        <Route path="/verify/:CODE" element={<User_verified/>}/>
         <Route path="*" element={<h1>Not Found</h1>}/>
       </Routes>
       </Router>
