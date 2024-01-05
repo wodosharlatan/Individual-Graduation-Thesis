5a4d8b028784c8d86eac02fbac5f5ebae9a1ba68
Tomáš Bosák <tombos255@gmail.com>
2024-01-05 03:37:16 +0100

    50ms delay

--- frontend/src/pages/email_responses/user_verified/user_verified.jsx
+++ frontend/src/pages/email_responses/user_verified/user_verified.jsx
@@ -3,24 +3,22 @@ import "./user_verified.css";
 
 function User_verified() {
 
-  // remove hash from url and redirect to home page
-  window.history.pushState("", "", "/");
+	setTimeout(function () {
+		window.history.pushState("", "", "/");
+	}, 50);
 
-  
-
-
-  return (
-    <>
-      <div class="centered">
-        <div class="center">
-          <h1>Vaše ověření proběhlo úspěšně</h1>
-        </div>
-        <div class="center">
-          <i class="uil uil-check-circle icon"></i>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div class="centered">
+				<div class="center">
+					<h1>Vaše ověření proběhlo úspěšně</h1>
+				</div>
+				<div class="center">
+					<i class="uil uil-check-circle icon"></i>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default User_verified;
