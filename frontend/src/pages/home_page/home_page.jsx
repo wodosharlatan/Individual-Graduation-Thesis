3ecdfb9d947e4e36236afdd261d5641a2e0debbf
Tomáš Bosák <tombos255@gmail.com>
2024-01-16 22:38:01 +0100

    Funguje to

--- frontend/src/pages/home_page/home_page.jsx
+++ frontend/src/pages/home_page/home_page.jsx
@@ -1,49 +1,37 @@
 import React, { useState, useEffect } from "react";
 
 function Home() {
-  const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/API/products");
-        const result = await response.json();
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("/API/products");
+				const result = await response.json();
 
+				setProducts(result);
+				console.log(result);
+			} catch (error) {
+				console.error(error);
+			}
+		};
 
-        setProducts(result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+		fetchData();
+	}, []);
 
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      {products.map((product) => (
-        <div key={product._id}>
-          <h1>{product.productName}</h1>
-          {product.productImage && (
-            <img
-              src={`data:image/png;base64,${arrayBufferToBase64(product.productImage.data)}`}
-              alt={`Image for ${product.productName}`}
-            />
-          )}
-        </div>
-      ))}
-    </>
-  );
-}
-
-// Helper function to convert ArrayBuffer to base64
-function arrayBufferToBase64(buffer) {
-  let binary = "";
-  const bytes = new Uint8Array(buffer);
-  for (let i = 0; i < bytes.byteLength; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return btoa(binary);
+	return (
+		<>
+			{products.map((product) => (
+				<div key={product._id}>
+					<h1>{product.productName}</h1>
+					<img
+						src={product.productImagePath}
+						alt={`Image for ${product.productName}`}
+					/>
+				</div>
+			))}
+		</>
+	);
 }
 
 export default Home;
