96fed5ee172c20800cc56fc211209ceffffff5df
Tomáš Bosák <tombos255@gmail.com>
2024-02-25 14:55:06 +0100

    Smoll fixes

--- server.sh
+++ server.sh
@@ -1,8 +1,16 @@
 cd frontend
+
+# >>> Will throw error if OS is not Linux, Used for Deployment > Can ignore if OS = WIN
 npm i
+#
+
 npm run build
 cd ..
 cp -r frontend/dist backend
+
+
+
+
 cd backend
 npm install
 npm audit fix
