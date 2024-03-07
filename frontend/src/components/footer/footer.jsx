4e12b67e194951a0f9d1a52692ac817bfcbdf0b2
markosak69 <marek.hejral@seznam.cz>
2024-03-07 09:45:56 +0100

    jo

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
 			<div className="contacts">
 				<h2>Kontakty</h2>
 				<p>+420 123 648 273</p>
-				<p>info@darkyzpedigu.cz</p>
-				<a className="linksoc" href="https://www.instagram.com/"><i className="uil uil-instagram"></i></a>
-				<a className="linksoc" href="https://www.facebook.com/groups/442086279211061/"><i className="uil uil-facebook-f"></i></a>
+				<p>darkyzpedigu@gmail.com</p>
+				<a target="_blank" className="linksoc" href="https://www.instagram.com/darkyzpedigu/"><i className="uil uil-instagram"></i></a>
+				<a target="_blank" className="linksoc" href="https://www.facebook.com/groups/442086279211061/"><i className="uil uil-facebook-f"></i></a>
 			</div>
 			<div className="delivery_payment">
 				<h2>Možnosti dopravy</h2>
