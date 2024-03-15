950a031110fc6827332b763d948b4a8d3b257e84
Tomáš Bosák <tombos255@gmail.com>
2024-03-16 00:51:10 +0100

    Objednavky Lets go

--- backend/routes/cart/checkout.js
+++ backend/routes/cart/checkout.js
@@ -10,135 +10,139 @@ const verify = require("../../functions/verify");
 const GenerateHash = require("../../functions/generate_hash");
 
 router.post("/", async (req, res) => {
-	const validProducts = [];
-	const TotalPrice = 0;
+  const validProducts = [];
+  let TotalPrice = 0;
 
-	try {
-		let Email = req.body.Email;
-		if (isNull(Email)) {
-			const VerificationCode = req.body.verificationCode;
-			if (isNull(VerificationCode)) {
-				return res
-					.status(400)
-					.json({ message: "Email or verification code are required" });
-			} else {
-				const user = await User.findOne({ VerificationCode: VerificationCode });
-				if (!user) {
-					return res.status(404).json({ message: "User not found" });
-				} else {
-					Email = user.Email;
-				}
-			}
-		}
-
-		const CheckoutPlace = req.body.CheckoutPlace;
-		if (isNull(CheckoutPlace)) {
-			return res.status(400).json({ message: "Checkout place is required" });
-		}
-		const FormOFPayment = req.body.FormOFPayment;
-		if (isNull(FormOFPayment)) {
-			return res.status(400).json({ message: "Form of payment is required" });
-		}
-
-		const user = await User.findOne({ Email: req.body.Email });
-		if (!user) {
-			return res.status(404).json({ message: "User not found" });
-		}
-
-		const requestedProducts = req.body.Products;
-		if (isNull(requestedProducts)) {
-			return res
-				.status(400)
-				.json({ message: "At least one product are required" });
-		}
-
-		for (let i = 0; i < requestedProducts.length; i++) {
-			const product = await Product.findOne({
-				productName: requestedProducts[i].productName,
-			});
-			if (!product) {
-				return res.status(404).json({ message: "Product not found" });
-			}
-			if (requestedProducts[i].Quantity < 1) {
-				return res.status(400).json({ message: "Quantity must be at least 1" });
-			}
-			product.ProductQuantity -= requestedProducts[i].Quantity;
-			await Product.FindOneAndUpdate(
-				{ productName: requestedProducts[i].productName },
-				product
-			);
-			validProducts.push(product);
-			TotalPrice += product.ProductPrice * requestedProducts[i].Quantity;
-		}
-
-		const OrderID = GenerateHash();
-		const order = new Order({
-			OrderProducts: validProducts,
-			Email: Email,
-			OrderID: OrderID,
-			CheckoutPlace: CheckoutPlace,
-			FormOFPayment: FormOFPayment,
-			TotalPrice: TotalPrice,
-		});
-
-		await order.save();
-		return res.status(200).json({ message: "Order placed successfully" });
-	} catch (error) {
-		return res.status(500).json({ message: error.toString() });
-	}
-});
-
-
-router.post("/get-all", async (req, res) => {
-    try {
-
-        const VerificationCode = req.body.VerificationCode;
-        if (isNull(VerificationCode)) {
-            return res.status(400).json({ message: "Verification code is required" });
+  try {
+    let Email = req.body.Email;
+    if (isNull(Email)) {
+      const VerificationCode = req.body.VerificationCode;
+      if (isNull(VerificationCode)) {
+        return res
+          .status(400)
+          .json({ message: "Email or verification code are required" });
+      } else {
+        const user = await User.findOne({ VerificationCode: VerificationCode });
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        } else {
+          Email = user.Email;
         }
-
-        console.log(await User.findOne({VerificationCode: VerificationCode}))
-
-		const result = await verify(VerificationCode);
-		if(result !== true) {
-			return res.status(400).json(result);
-		}
-
-        const orders = await Order.find();
-        return res.status(200).json(orders);
-    } catch (error) {
-        return res.status(500).json({ message: error.toString() });
+      }
     }
 
+    const CheckoutPlace = req.body.CheckoutPlace;
+    if (isNull(CheckoutPlace)) {
+      return res.status(400).json({ message: "Checkout place is required" });
+    }
+    const FormOFPayment = req.body.FormOFPayment;
+    if (isNull(FormOFPayment)) {
+      return res.status(400).json({ message: "Form of payment is required" });
+    }
+
+    const requestedProducts = req.body.Products;
+    if (isNull(requestedProducts)) {
+      return res
+        .status(400)
+        .json({ message: "At least one product are required" });
+    }
+
+    for (let i = 0; i < requestedProducts.length; i++) {
+      const product = await Product.findOne({
+        productName: requestedProducts[i].productName,
+      });
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      if (requestedProducts[i].productQuantity < 1) {
+        return res.status(400).json({ message: "Quantity must be at least 1" });
+      }
+
+      if (requestedProducts[i].productQuantity > product.productQuantity) {
+        return res.status(400).json({ message: "Not enough products" });
+      }
+
+      const availableProducts =
+        product.productQuantity - requestedProducts[i].productQuantity;
+
+      await Product.findOneAndUpdate(
+        { productName: requestedProducts[i].productName },
+        {
+          $set: {
+            productQuantity: availableProducts,
+          },
+        }
+      );
+
+	  product.productQuantity = requestedProducts[i].productQuantity;
+
+      TotalPrice += product.productPrice * requestedProducts[i].productQuantity;
+      validProducts.push(product);
+    }
+
+    const OrderID = GenerateHash();
+    const order = new Order({
+      OrderProducts: validProducts,
+      Email: Email,
+      OrderID: OrderID,
+      CheckoutPlace: CheckoutPlace,
+      FormOFPayment: FormOFPayment,
+      TotalPrice: TotalPrice,
+    });
+
+    await order.save();
+    return res.status(200).json({ message: "Order placed successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.toString() });
+  }
+});
+
+router.post("/get-all", async (req, res) => {
+  try {
+    const VerificationCode = req.body.VerificationCode;
+    if (isNull(VerificationCode)) {
+      return res.status(400).json({ message: "Verification code is required" });
+    }
+
+    const result = await verify(VerificationCode);
+    if (result !== true) {
+      return res.status(400).json(result);
+    }
+
+    const orders = await Order.find();
+    return res.status(200).json(orders);
+  } catch (error) {
+    return res.status(500).json({ message: error.toString() });
+  }
 });
 
 router.post("/get-one/:ID", async (req, res) => {
-    try {
-        const VerificationCode = req.body.VerificationCode;
-        if (isNull(VerificationCode)) {
-            return res.status(400).json({ message: "Verification code is required" });
-        }
-
-        const user = await User.findOne({ VerificationCode: VerificationCode });
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const order = await Order.findOne({ OrderID: req.params.ID });
-        if (!order) {
-            return res.status(404).json({ message: "Order not found" });
-        }
-
-        if (order.Email !== user.Email) {
-            return res.status(403).json({ message: "You are not the owner of this order" });
-        }
-
-
-        return res.status(200).json(order);
-    } catch (error) {
-        return res.status(500).json({ message: error.toString() });
+  try {
+    const VerificationCode = req.body.VerificationCode;
+    if (isNull(VerificationCode)) {
+      return res.status(400).json({ message: "Verification code is required" });
     }
-});
 
+    const user = await User.findOne({ VerificationCode: VerificationCode });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const order = await Order.findOne({ OrderID: req.params.ID });
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    if (order.Email !== user.Email) {
+      return res
+        .status(403)
+        .json({ message: "You are not the owner of this order" });
+    }
+
+    return res.status(200).json(order);
+  } catch (error) {
+    return res.status(500).json({ message: error.toString() });
+  }
+});
 
 module.exports = router;
