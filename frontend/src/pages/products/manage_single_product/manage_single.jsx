a610b7c7e14891e78e370b4e2236ee715d5ae118
markosak69 <marek.hejral@seznam.cz>
2024-03-16 04:10:23 +0100

    oki

--- frontend/src/pages/products/manage_single_product/manage_single.jsx
+++ frontend/src/pages/products/manage_single_product/manage_single.jsx
@@ -47,7 +47,6 @@ function Manage_Single_Product() {
 			const cookies = document.cookie.split(";");
 			for (let i = 0; i < cookies.length; i++) {
 				const cookie = cookies[i].trim();
-				// Does this cookie string begin with the name we want?
 				if (cookie.substring(0, name.length + 1) === name + "=") {
 					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
 					break;
