2123966020a9581cc97774443fd19d2f8f8b76a5
markosak69 <marek.hejral@seznam.cz>
2024-03-16 11:43:36 +0100

    ok brut

--- frontend/src/pages/delivery_summary/delivery_summary.jsx
+++ frontend/src/pages/delivery_summary/delivery_summary.jsx
@@ -3,8 +3,6 @@ import "./_mainstyle.scss";
 import Footer from "../../components/footer/footer";
 import { Helmet } from "react-helmet";
 
-
-
 function Delivery_summary() {
   return (
     <>
@@ -28,12 +26,128 @@ function Delivery_summary() {
       <h1 className="nadpis_del">Shrnutí objednávky</h1>
       <hr></hr>
       <div className="div_del_sum_grid">
-        
+        <div className="cartitems_sum">
+          <img className="cartimg_sum" src="ceskaposta.svg"></img>
+          <div className="cartname_sum">
+            <p>Název Produktu</p>
+          </div>
+          <div className="cartquantity_sum">
+            <p>
+              <input type="number" min={1}></input>
+            </p>
+          </div>
+
+          <div className="cartdelete_sum">
+            <p>Odebrat</p>
+          </div>
+          <div className="cartprice_sum">
+            <p>Cena: 1432,- kč/ks</p>
+          </div>
+        </div>
+
+        <div className="summary_data">
+          <h1 className="nadpis1_sum">Doručovací údaje</h1>
+          <hr></hr>
+          <div>
+            <div>
+              <p>Jméno a Příjmení</p>
+              <input type="text" placeholder="Marek Hejral"></input>
+            </div>
+            <div>
+              <p>Email</p>
+              <input type="text" placeholder="darkyzpedigu@gmail.com"></input>
+            </div>
+            <div>
+              <p>Tel. číslo</p>
+              <input type="text" placeholder="607 843 324"></input>
+            </div>
+            <div>
+              <p>Ulice a čp.</p>
+              <input type="text" placeholder="Vrchlabí 574"></input>
+            </div>
+            <div>
+              <p>PSČ</p>
+              <input type="text" placeholder="518 93"></input>
+            </div>
+            <div>
+              <p>Město</p>
+              <input type="text" placeholder="Vrchlabí"></input>
+            </div>
+            <div>
+              <br></br>
+              <h1>Způsob doručení</h1>
+              <hr></hr>
+              <input type="text" placeholder="Česká pošta"></input>
+            </div>
+          </div>
+        </div>
+
+        <div className="cartitems_sum">
+          <img className="cartimg_sum" src="ceskaposta.svg"></img>
+          <div className="cartname_sum">
+            <p>Název Produktu</p>
+          </div>
+          <div className="cartquantity_sum">
+            <p>
+              <input type="number" min={1}></input>
+            </p>
+          </div>
+
+          <div className="cartdelete_sum">
+            <p>Odebrat</p>
+          </div>
+          <div className="cartprice_sum">
+            <p>Cena: 1432,- kč/ks</p>
+          </div>
+        </div>
+
+        <div className="cartitems_sum">
+          <img className="cartimg_sum" src="ceskaposta.svg"></img>
+          <div className="cartname_sum">
+            <p>Název Produktu</p>
+          </div>
+          <div className="cartquantity_sum">
+            <p>
+              <input type="number" min={1}></input>
+            </p>
+          </div>
+
+          <div className="cartdelete_sum">
+            <p>Odebrat</p>
+          </div>
+          <div className="cartprice_sum">
+            <p>Cena: 1432,- kč/ks</p>
+          </div>
+        </div>
+
+        <div className="cartitems_sum">
+          <img className="cartimg_sum" src="ceskaposta.svg"></img>
+          <div className="cartname_sum">
+            <p>Název Produktu</p>
+          </div>
+          <div className="cartquantity_sum">
+            <p>
+              <input type="number" min={1}></input>
+            </p>
+          </div>
+
+          <div className="cartdelete_sum">
+            <p>Odebrat</p>
+          </div>
+          <div className="cartprice_sum">
+            <p>Cena: 1432,- kč/ks</p>
+          </div>
+        </div>
       </div>
 
-      <div onClick={() => {handleAccept();}} className="nextbtn">
-				<p>Dokončit a zaplatit</p>
-			</div>
+      <div
+        onClick={() => {
+          handleAccept();
+        }}
+        className="nextbtn"
+      >
+        <p>Dokončit a zaplatit</p>
+      </div>
 
       <Footer />
     </>
